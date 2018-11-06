c2778d4a07bc8de1b4c4e5d9787524003b6ba624
Patrick Sewell <pssewell21@outlook.com>
2018-11-06 10:37:51 -0600

    Adding sprite frames

--- HW3/game.lua
+++ HW3/game.lua
@@ -26,6 +26,8 @@ end
 function scene:create( event )
 	local sceneGroup = self.view
 
+	display.newRect(160, 50, 300, 300)
+
 	-- set up the image sheet coordinates for animations 
 	local options =
 	{
@@ -34,17 +36,28 @@ function scene:create( event )
 			{ x = 1068, y = 0, width = 260, height = 200 }, -- 1 - Red Square
 			{ x = 810, y = 0, width = 260, height = 200 },  -- 2 - House 2
 			{ x = 550, y = 0, width = 260, height = 200 },  -- 3 - House 1
-			{ x = 368, y = 8, width = 41, height = 41 },  -- 4 - Good Pick Icon
-			{ x = 413, y = 10, width = 35, height = 35 },  -- 5 - Bad Pick Icon
-			{ x = 148, y = 23, width = 39, height = 36 },  -- 6 - Bird 1
-			{ x = 189, y = 23, width = 43, height = 34 }  -- 7 - Bird 2
+			{ x = 368, y = 8, width = 41, height = 41 },  -- 4 - Green Circle
+			{ x = 413, y = 10, width = 35, height = 35 },  -- 5 - Red X
+			{ x = 148, y = 22, width = 41, height = 38 },  -- 6 - Bird 1
+			{ x = 191, y = 22, width = 39, height = 36 },  -- 7 - Bird 2
+			{ x = 379, y = 122, width = 17, height = 41 },  -- 8 - Bottle 1
+			{ x = 403, y = 122, width = 17, height = 41 },  -- 9 - Bottle 2
+			{ x = 429, y = 115, width = 36, height = 21 },  -- 10 - Hat 1
+			{ x = 429, y = 137, width = 36, height = 21 },  -- 11 - Hat 2
+			{ x = 429, y = 159, width = 36, height = 21 },  -- 12 - Hat 3
+			{ x = 473, y = 111, width = 24, height = 25 },  -- 13 - Cup 1
+			{ x = 473, y = 136, width = 24, height = 23 },  -- 14 - Cup 2
+			{ x = 473, y = 160, width = 24, height = 23 },  -- 15 - Cup 3
+			{ x = 511, y = 93, width = 22, height = 31 },  -- 16 - Pot 1
+			{ x = 512, y = 125, width = 22, height = 32 },  -- 17 - Pot 2
+			{ x = 510, y = 158, width = 22, height = 32 }  -- 18 - Pot 3
 		}
 	}
 	
 	-- initialize the image sheet
 	local sheet = graphics.newImageSheet("marioware.png", options)
 
-	local image = display.newImage(sheet, 7, 200, 100)
+	local image = display.newImage(sheet, 18, 200, 100)
 	image.xScale = 3
 	image.yScale = 3
 
