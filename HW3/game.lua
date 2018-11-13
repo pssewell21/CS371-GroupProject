48b021009aa570b6bc0172176ef2573e074b9abc
Patrick Sewell <pssewell21@outlook.com>
2018-11-12 22:43:23 -0600

    intermediate check in

--- HW3/game.lua
+++ HW3/game.lua
@@ -11,7 +11,7 @@ local widget = require( "widget" )
 local physics = require ("physics") -- AA
 local scene = composer.newScene()
 
-local stageNumber = 2
+local stageNumber = 3
 local numberOfStages = 10
 
 local itemToFindIndex = 0
@@ -411,27 +411,27 @@ function scene:show( event )
 				sceneGroup:insert(item5)
 			end
 		elseif (stageNumber == 3) then
-			local item1 = getImage(itemsInHouse[1], 240, 283, true)
+			local item1 = getImage(itemsInHouse[1], 240, 348, true)
 			itemsToRemove[1] = item1
 			sceneGroup:insert(item1)
 
-			local item2 = getImage(itemsInHouse[2], 240, 348, true)
+			local item2 = getImage(itemsInHouse[2], 240, 430, true)
 			itemsToRemove[2] = item2
 			sceneGroup:insert(item2)
 
-			local item3 = getImage(itemsInHouse[3], 130, 290, true)
+			local item3 = getImage(itemsInHouse[3], 130, 358, true)
 			itemsToRemove[3] = item3
 			sceneGroup:insert(item3)
 
 			-- Check for nil value before attempting to add the item to the view
 			if itemsInHouse[4] ~= nil then
-				local item4 = getImage(itemsInHouse[4], 100, 290, true)
+				local item4 = getImage(itemsInHouse[4], 100, 358, true)
 				itemsToRemove[4] = item4
 				sceneGroup:insert(item4)
 			end
 
 			if itemsInHouse[5] ~= nil then
-				local item5 = getImage(itemsInHouse[5], 240, 219, true)
+				local item5 = getImage(itemsInHouse[5], 240, 308, true)
 				itemsToRemove[5] = item5
 				sceneGroup:insert(item5)
 			end
