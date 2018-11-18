01c7609096fc9b5b55170906df1788c8586bfb75
Patrick Sewell <pssewell21@outlook.com>
2018-11-17 23:08:30 -0600

    Adding rudimentary level 1 implementation

--- inSANiTy- CS 371 Group Project/game.lua
+++ inSANiTy- CS 371 Group Project/game.lua
@@ -3,6 +3,9 @@ local widget = require( "widget" )
 local scene = composer.newScene()
 
 local roboBlock
+local level
+
+local levelMovementSpeed = 25
  
 -- -----------------------------------------------------------------------------------
 -- Code outside of the scene event functions below will only be executed ONCE unless
@@ -39,6 +42,67 @@ local function screenTouched(event)
     jumpUp()
     timer.performWithDelay(150, jumpDown)
 end
+
+local function moveLevel()
+    transition.to(level, 
+    {
+        time=150, 
+        x = level.x - levelMovementSpeed,
+        onComplete = 
+            function() 
+                moveLevel()
+            end
+    })
+end
+
+local function buildLevel()
+    local group = display.newGroup()
+
+    local floor = display.newRect(-50, 210 + 3, 50000, 110)
+    floor.strokeWidth = 3
+    floor:setStrokeColor(1, 0, 0)
+    floor:setFillColor(0, 0, 0, 0.75)
+    floor.anchorX = 0
+    floor.anchorY = 0
+    group:insert(floor)
+
+    local vertices = {0,0, 30,0, 15,-30}
+
+    local item1 = display.newPolygon(250, 180 - 3, vertices)
+    item1.strokeWidth = 3
+    item1:setStrokeColor(1, 1, 1)
+    item1:setFillColor(0, 0, 0, 0.75)
+    item1.anchorX = 0
+    item1.anchorY = 0
+    group:insert(item1)
+
+    local item2 = display.newPolygon(400, 180 - 3, vertices)
+    item2.strokeWidth = 3
+    item2:setStrokeColor(1, 1, 1)
+    item2:setFillColor(0, 0, 0, 0.75)
+    item2.anchorX = 0
+    item2.anchorY = 0
+    group:insert(item2)
+
+    local item3 = display.newPolygon(800, 180 - 3, vertices)
+    item3.strokeWidth = 3
+    item3:setStrokeColor(1, 1, 1)
+    item3:setFillColor(0, 0, 0, 0.75)
+    item3.anchorX = 0
+    item3.anchorY = 0
+    group:insert(item3)
+
+    local item4 = display.newPolygon(1200, 180 - 3, vertices)
+    item4.strokeWidth = 3
+    item4:setStrokeColor(1, 1, 1)
+    item4:setFillColor(0, 0, 0, 0.75)
+    item4.anchorX = 0
+    item4.anchorY = 0
+    group:insert(item4)
+
+    return group
+end
+
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
 -- -----------------------------------------------------------------------------------
@@ -81,10 +145,13 @@ function scene:create( event )
     -- Change the button's label text
     -- ------------------------------
     nextSceneButton:setLabel( "NEXT" )
-    nextSceneButton:addEventListener("tap", gotoNextScene)   
+    nextSceneButton:addEventListener("tap", gotoNextScene)  
+
+    level = buildLevel() 
 
     sceneGroup:insert(background)
     sceneGroup:insert(nextSceneButton)
+    sceneGroup:insert(level)
 end 
  
 -- show()
@@ -94,10 +161,12 @@ function scene:show( event )
  
     if ( phase == "will" ) then
         -- Code here runs when the scene is still off screen (but is about to come on screen) 
-        roboBlock = display.newRect(0, 200, 30, 30)
+        roboBlock = display.newRect(0, 180, 30, 30)
         roboBlock.strokeWidth = 3
         roboBlock:setStrokeColor(1, 1, 1)
         roboBlock:setFillColor(0, 0, 0, 0.75)
+        roboBlock.anchorX = 0
+        roboBlock.anchorY = 0
 
         sceneGroup:insert(roboBlock)
 
@@ -105,6 +174,9 @@ function scene:show( event )
 
     elseif ( phase == "did" ) then
         -- Code here runs when the scene is entirely on screen 
+        timer.performWithDelay()
+        moveLevel()
+        moveLevel()
     end
 end 
  
