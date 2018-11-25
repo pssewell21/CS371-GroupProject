29dc4049ceeb8ac6816aa86a59c7dbaf497f6cae
Patrick Sewell <pssewell21@outlook.com>
2018-11-25 17:44:36 -0600

    Making scene transitions consistent

--- inSANiTy- CS 371 Group Project/game.lua
+++ inSANiTy- CS 371 Group Project/game.lua
@@ -71,7 +71,7 @@ local monsterGroup = display.newGroup()
 local function gotoNextScene()
     local sceneTransitionsOptions = 
     {
-        effects = "fade",
+        effects = "crossfade",
         time = 500,
     }
 
@@ -93,7 +93,7 @@ end
 local function gotoMenuScene()
     local sceneTransitionsOptions = 
     {
-        effects = "fade",
+        effects = "crossfade",
         time = 500,
     }
 
